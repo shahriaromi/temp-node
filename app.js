9e7f5f8fdbdf555501e729e8d38b4afbcdba1b8f
shahriaromi <shahriaromi10@gmail.com>
2022-06-24 16:01:56 +0600

    second commit

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-const os = require('os');
-
-//info current user
-// console.log(os.userInfo());
-
-//method returns system uptime in seconds
-// console.log(`system uptime in seconds: ${os.uptime()}`);
-
-const currentOS = {
-  name: os.type(),
-  release: os.release(),
-  totalMem: os.totalmem(),
-  freeMem: os.freemem(),
-};
-console.log(currentOS);
+console.log(123);
